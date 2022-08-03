5e34c8aaed0df8d6b55d2a8bb06c6a342e3cd2ec
Tiago Lustosa Santos <tiagolustosasantos@hotmail.com>
2022-08-03 15:30:54 -0300

    changes in decrypt_scripts.sh

--- .github/scripts/decrypt_scripts.sh
+++ .github/scripts/decrypt_scripts.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$ANDROID_KEYS_ZIP_PASSPHRASE" \
---output android/key_files.zip android/key_files.zip.gpg && cd android && jar xvf key_files.zip && cd -
-ls -d $PWD/android/*
+gpg --quiet --batch --yes --pinentry loopback --passphrase="$ANDROID_KEYS_ZIP_PASSPHRASE" --output android/key_files.zip --decrypt android/key_files.zip.gpg  \
+ cd android && jar xvf key_files.zip && cd -
 mv ./android/upload-keystore.jks ./android/app
 # move your file according to path in key.properties
